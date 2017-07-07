a4f0296ba89786bfad2ddabf640e7da38b987545
zhao <649038269@qq.com>
2017-07-07 13:16:14 +0800

    Update server

--- src/tunnel_server.py
+++ src/tunnel_server.py
@@ -1,8 +1,11 @@
 # Echo server program
 import socket
+import sys
 import logging
+from http import HTTPStatus
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 HOST = ''  # Symbolic name meaning all available interfaces
 PORT = 8888  # Arbitrary non-privileged port
@@ -12,11 +15,11 @@ def client(host, port, content):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((host, port))
         s.sendall(content)
-        data = s.recv(1024)
+        data = s.recv(65537)
         while data:
             print('client data', data)
             yield data
-            data = s.recv(1024)
+            data = s.recv(65537)
 
 
 def server(host, port):
@@ -29,7 +32,7 @@ def server(host, port):
             print('Connected by', addr)
             # content = b''
             while True:
-                data = conn.recv(1024)
+                data = conn.recv(65537)
                 if not data:
                     break
                 print('data', data)
@@ -38,5 +41,67 @@ def server(host, port):
                     conn.sendall(content)
 
 
+class TunnelHTTPRequestHandler(BaseHTTPRequestHandler):
+    def handle_one_request(self):
+        """Handle a single HTTP request.
+
+        You normally don't need to override this method; see the class
+        __doc__ string for information on how to handle specific HTTP
+        commands such as GET and POST.
+
+        """
+        try:
+            self.raw_requestline = self.rfile.readline(65537)
+            if len(self.raw_requestline) > 65536:
+                self.requestline = ''
+                self.request_version = ''
+                self.command = ''
+                self.send_error(HTTPStatus.REQUEST_URI_TOO_LONG)
+                return
+            if not self.raw_requestline:
+                self.close_connection = True
+                return
+            if not self.parse_request():
+                # An error code has been sent, just exit
+                return
+            mname = 'do_' + self.command
+            if not hasattr(self, mname):
+                self.send_error(
+                    HTTPStatus.NOT_IMPLEMENTED,
+                    "Unsupported method (%r)" % self.command)
+                return
+            method = getattr(self, mname)
+            method()
+            self.wfile.flush()  # actually send the response if not already done.
+        except socket.timeout as e:
+            # a read or a write timed out.  Discard this connection
+            self.log_error("Request timed out: %r", e)
+            self.close_connection = True
+            return
+
+
+def test(HandlerClass=BaseHTTPRequestHandler,
+         ServerClass=HTTPServer, protocol="HTTP/1.0", port=8000, bind=""):
+    """Test the HTTP request handler class.
+
+    This runs an HTTP server on port 8000 (or the port argument).
+
+    """
+    server_address = (bind, port)
+
+    HandlerClass.protocol_version = protocol
+    httpd = ServerClass(server_address, HandlerClass)
+
+    sa = httpd.socket.getsockname()
+    logging.info("Serving HTTP on", sa[0], "port", sa[1], "...")
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        logging.info("\nKeyboard interrupt received, exiting.")
+        httpd.server_close()
+        sys.exit(0)
+
+
 if __name__ == '__main__':
-    server(HOST, PORT)
+    test(TunnelHTTPRequestHandler)
+
